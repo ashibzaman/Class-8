b14b17c9ac5fcb756ce37ea10ba703e6b5d9da20
ashibzaman <118059494+ashibzaman@users.noreply.github.com>
2022-11-13 16:43:41 +0600

    Add files via upload

---
+++ controller/test.php
@@ -0,0 +1,27 @@
+<?php
+
+echo "<pre>";
+print_r ($_POST);
+
+echo "</pre>";
+
+
+
+echo "Initialization/ Loop: <br>";
+            for ($i=1; $i<11; $i++)
+            {echo "Hellow world <br>";}
+
+            echo "<br>";
+
+            for ($i=1; $i<=10; $i++)
+            {echo $i . "<br>";}
+
+
+            $start = 2022;
+            $end = 1950;
+
+            for ($i= 2000; $i>= 1900; $i--)
+            {echo "<option>$i</option>";}
+
+
+
